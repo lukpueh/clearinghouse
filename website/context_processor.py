0479289d9f1833dd524bd1b63c17ede1816636eb
Lukas Puehringer <lukas.puehringer@nyu.edu>
2017-08-16 12:36:03 -0400

    Enhance docstring/comments in context_processor
    Following @vladimir-v-diaz's review comment this change
    makes the docstring and comments of the custom options
    context_processor clearer.

--- website/context_processor.py
+++ website/context_processor.py
@@ -11,10 +11,14 @@ def customizable_strings(request):
 }
 
 def options(request):
-    """Make variables defined in settings.py available to the template engine."""
+    """ If 'clearinghouse.website.context_processor.options'
+    is added to TEMPLATE_CONTEXT_PROCESSORS in settings.py the items of the
+    returned dict can be directly accessed in the template engine.
+    Currently we only make the boolean DEBUG setting available."""
+
     from django.conf import settings
     return {
-        # A debug variable is available to the templates via django's
+        # A debug variable is available to the templates via django's built-in
         # 'django.core.context_processors.debug', but only if the request's
         # IP address is listed in the INTERNAL_IPS setting, c.f.:
         # https://docs.djangoproject.com/en/1.9/ref/templates/api/#django-template-context-processors-debug
